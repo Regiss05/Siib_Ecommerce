357c39267889da014faa785c48e30fcf7f959b10
regiss05 <regiss.steven@gmail.com>
2025-02-27 21:03:12 +0300

    add languages

--- frontend/src/Shop/components/Header.tsx
+++ frontend/src/Shop/components/Header.tsx
@@ -1,21 +1,15 @@
-import React, { CSSProperties } from "react";
+import React, { CSSProperties, useState } from "react";
+import { useTranslation } from "react-i18next";
 import { User } from "./Home";
 import { styled, alpha } from '@mui/material/styles';
 import SearchIcon from '@mui/icons-material/Search';
 import InputBase from '@mui/material/InputBase';
 import Menu, { MenuProps } from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
-import EditIcon from '@mui/icons-material/Edit';
-import Divider from '@mui/material/Divider';
-import ArchiveIcon from '@mui/icons-material/Archive';
-import FileCopyIcon from '@mui/icons-material/FileCopy';
-import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import { Button } from "@mui/material";
-import luangage from '../../imges/statics/language.svg';
+import languageIcon from '../../imges/statics/language.svg';
 import shopsloc from '../../imges/statics/shopsloc.svg';
 import hand from '../../imges/statics/hand.svg';
-import { ClassNames } from "@emotion/react";
 import Box from '@mui/material/Box';
 import locationshop from '../../imges/statics/locationshop.svg';
 
@@ -23,7 +17,6 @@ const Search = styled('div')(() => ({
   position: 'relative',
   borderRadius: '10px',
   backgroundColor: 'white',
-  marginLeft: 0,
   width: '80%',
 }));
 
@@ -35,7 +28,7 @@ const SearchIconWrapper = styled('div')(({ theme }) => ({
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'center',
-  color: 'black', // Set the color of the search icon to black
+  color: 'black',
 }));
 
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
@@ -45,11 +38,8 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
     paddingLeft: `calc(1em + ${theme.spacing(4)})`,
     transition: theme.transitions.create('width'),
     width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: '20ch',
-    },
     '&::placeholder': {
-      color: 'black', // Change the placeholder color to black
+      color: 'black',
     },
   },
 }));
@@ -57,7 +47,7 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
 interface Props {
   onSignIn: () => void;
   onSignOut: () => void;
-  user: User | null
+  user: User | null;
 }
 
 const headerStyle: CSSProperties = {
@@ -68,86 +58,42 @@ const headerStyle: CSSProperties = {
   borderBottomLeftRadius: '30px',
 };
 
-const StyledMenu = styled((props: MenuProps) => (
-  <Menu
-    elevation={0}
-    anchorOrigin={{
-      vertical: 'bottom',
-      horizontal: 'right',
-    }}
-    transformOrigin={{
-      vertical: 'top',
-      horizontal: 'right',
-    }}
-    {...props}
-  />
-))(({ theme }) => ({
-  '& .MuiPaper-root': {
-    borderRadius: 6,
-    marginTop: theme.spacing(1),
-    minWidth: 180,
-    color: 'rgb(55, 65, 81)',
-    boxShadow:
-      'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
-    '& .MuiMenu-list': {
-      padding: '4px 0',
-    },
-    '& .MuiMenuItem-root': {
-      '& .MuiSvgIcon-root': {
-        fontSize: 18,
-        color: theme.palette.text.secondary,
-        marginRight: theme.spacing(1.5),
-      },
-      '&:active': {
-        backgroundColor: alpha(
-          theme.palette.primary.main,
-          theme.palette.action.selectedOpacity,
-        ),
-      },
-    },
-    ...theme.applyStyles('dark', {
-      color: theme.palette.grey[300],
-    }),
-  },
-}));
-
-
 export default function Header(props: Props) {
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const { t, i18n } = useTranslation();
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
+
   const handleClick = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget);
   };
-  const handleClose = () => {
+
+  const handleClose = (lang?: string) => {
     setAnchorEl(null);
+    if (lang) i18n.changeLanguage(lang);
   };
 
   return (
     <header style={headerStyle} className="header">
-      <Box sx={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: '5px'}}>
+      <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: '5px' }}>
         <div>
           {props.user === null ? (
-            <button className="signin" onClick={props.onSignIn}>Sign in</button>
+            <button className="signin" onClick={props.onSignIn}>{t("signIn")}</button>
           ) : (
-            <div>
-              <button className="signout" type="button" onClick={props.onSignOut}>Sign out</button>
-            </div>
+            <button className="signout" onClick={props.onSignOut}>{t("signOut")}</button>
           )}
         </div>
-        <img className="icohome" src={luangage} alt="language" />
+        <img className="icohome" src={languageIcon} alt={t("language")} />
       </Box>
       <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
         <Box sx={{ fontSize: '18px', color: '#eeecfe' }}>
-          Hello, <img className='handico waving-hand' src={hand} alt="hand" />
+          {t("hello")}, <img className='handico waving-hand' src={hand} alt="hand" />
           <div>
-          {props.user === null ? (
-            <div>Dear</div>
-          ) : (
-            <div className="userlogin">
-              @{props.user.username}
-            </div>
-          )}
-        </div>
+            {props.user === null ? (
+              <div>{t("dear")}</div>
+            ) : (
+              <div className="userlogin">@{props.user.username}</div>
+            )}
+          </div>
         </Box>
         <Box sx={{ display: 'flex', flexDirection: 'column', gap: '5px' }}>
           <img className="icohome" src={locationshop} alt="sort" />
@@ -160,37 +106,21 @@ export default function Header(props: Props) {
             <SearchIcon />
           </SearchIconWrapper>
           <StyledInputBase
-            placeholder="Search…"
+            placeholder={t("search")}
             inputProps={{ 'aria-label': 'search' }}
           />
         </Search>
         <div>
           <Button
             sx={{ backgroundColor: '#eeecfe', color: 'black', borderRadius: '10px', width: '30px' }}
-            id="demo-customized-button"
-            aria-controls={open ? 'demo-customized-menu' : undefined}
-            aria-haspopup="true"
-            aria-expanded={open ? 'true' : undefined}
-            variant="contained"
-            disableElevation
             onClick={handleClick}
-            endIcon={<KeyboardArrowDownIcon />}
           >
-            EN
+            {i18n.language.toUpperCase()}
           </Button>
-          <StyledMenu
-            id="demo-customized-menu"
-            MenuListProps={{
-              'aria-labelledby': 'demo-customized-button',
-            }}
-            anchorEl={anchorEl}
-            open={open}
-            onClose={handleClose}
-          >
-            <MenuItem onClick={handleClose} disableRipple>
-              FR
-            </MenuItem>
-          </StyledMenu>
+          <Menu anchorEl={anchorEl} open={open} onClose={() => handleClose()}>
+            <MenuItem onClick={() => handleClose('en')}>{t("english")}</MenuItem>
+            <MenuItem onClick={() => handleClose('fr')}>{t("french")}</MenuItem>
+          </Menu>
         </div>
       </div>
     </header>
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import { I18nextProvider } from 'react-i18next';
+import i18n from './locales/i18n';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
 import App from './Shop';
-import "./i18n.js";
 
 import './index.css';
 // import Shop from './Shop';
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>  {/* Wrap App with BrowserRouter */}
-    <App />
+    <I18nextProvider i18n={i18n}>
+      <App />
+    </I18nextProvider>
   </BrowserRouter>
 );
