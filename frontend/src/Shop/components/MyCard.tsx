0e051221621236b7dd0c3d7a6ab7586c2b79b8b9
mass1993 <benjaminnyantora@gmail.com>
2025-02-28 10:31:46 +0200

    feature add items

--- frontend/src/Shop/components/MyCard.tsx
+++ frontend/src/Shop/components/MyCard.tsx
@@ -17,12 +17,40 @@ import car12 from '../../imges/statics/car12.jpg';
 import car13 from '../../imges/statics/car13.jpg';
 import car14 from '../../imges/statics/car14.jpg';
 import car15 from '../../imges/statics/car15.jpg';
-import car16 from '../../imges/statics/car16.jpg';
+import v92024 from '../../imges/statics/v82024.png';
 import house2 from '../../imges/statics/house2.jpg';
 import house3 from '../../imges/statics/house3.jpg';
 import house4 from '../../imges/statics/house4.jpg';
 import house5 from '../../imges/statics/house5.jpg';
-import house from '../../imges/statics/house1.jpg';
+import house6 from '../../imges/statics/house6.jpg';
+import house7 from '../../imges/statics/house7.jpg';
+import house8 from '../../imges/statics/house8.jpg';
+import house9 from '../../imges/statics/house9.jpg';
+import house10 from '../../imges/statics/house10.jpg';
+import house11 from '../../imges/statics/house11.jpg';
+import house12 from '../../imges/statics/house12.jpg';
+import house13 from '../../imges/statics/house13.jpg';
+import house14 from '../../imges/statics/house14.jpg';
+import car17 from '../../imges/statics/car17.jpg';
+import car18 from '../../imges/statics/car18.jpg';
+import car19 from '../../imges/statics/car19.jpg';
+import car20 from '../../imges/statics/car20.jpg';
+import car33 from '../../imges/statics/car33.jpg';
+import houses from '../../imges/statics/housenew2.jpg';
+import vpcar from '../../imges/statics/landcruser.jpg';
+import jeep from '../../imges/statics/jeep.png';
+import landlover from '../../imges/statics/landlover.png';
+import vpcar2 from '../../imges/statics/v82024.png';
+import crown from '../../imges/statics/crown2024.png';
+import benz from '../../imges/statics/benz.png';
+import benz2 from '../../imges/statics/benz2.png';
+import prado from '../../imges/statics/toyotaprado.png';
+import house21 from '../../imges/statics/house21.png';
+import house22 from '../../imges/statics/house22.png';
+import house23 from '../../imges/statics/house23.png';
+import house24 from '../../imges/statics/house24.png';
+import house25 from '../../imges/statics/house25.png';
+import house26 from '../../imges/statics/house26.png';
 import onlinestore from '../../imges/statics/onlinestore.svg';
 interface Product {
   id: number;
@@ -34,16 +62,16 @@ interface Product {
 // Sample Product Data
 const Product = [
   { id: 1, name: "car1", price: "0.10", image: car1, shopName: "SIIB IVORY COST"},
-  { id: 2, name: "car2", price: "0.9", image: car2, shopName: "SIIB IVORY COST" },
+  { id: 2, name: "car2", price: "0.9", image: car2, shopName: "SIIB RWANDA" },
   { id: 3, name: "car3", price: "0.03", image: car3, shopName: "SIIB BURUNDI" },
   { id: 4, name: "Smartwatch", price: "0.06", image:car4, shopName: "SIIB BURUNDI"},
   { id: 5, name: "Camera", price: "0.003", image: car5, shopName: "SIIB BURUNDI" },
   { id: 6, name: "Monitor", price: "0.001", image: car6, shopName: "SIIB BURUNDI"},
-  { id: 8, name: "Emerald Paradise", price: "0.002", image:house, shopName: "SIIB BURUNDI"},
+  { id: 8, name: "Emerald Paradise", price: "0.002", image:house6, shopName: "SIIB RWANDA"},
   { id: 9, name: "Lotus Feet Cottage", price: "0.005", image:house2, shopName: "Tech Hub"},
   { id: 10, name: "Divine Heritage", price: "0.0021", image:house3, shopName: "SIIB TANZANIA"},
   { id: 11, name: "Moonlit Roof Cottage", price: "0.0011", image:house4, shopName: "SIIB TANZANIA"},
-  { id: 12, name: "Pure Abode", price: "0.0013", image:house5, shopName: "SIIB TANZANIA"},
+  { id: 12, name: "Pure Abode", price: "0.0013", image:house5, shopName: "SIIB RWANDA"},
   { id: 13, name: "LandLover", price: "0.0017", image:car7, shopName: "SIIB TANZANIA"},
   { id: 14, name: "Mercedes-Benz AMG G-Class", price: "0.0025", image:car8, shopName: "SIIB TANZANIA"},
   { id: 15, name: "BYD Shark Premium PHEV ", price: "0.0027", image:car9, shopName: "SIIB TANZANIA"},
@@ -53,7 +81,36 @@ const Product = [
   { id: 19, name: "Hongqi E-HS9 2025", price: "0.0028", image:car13, shopName: "SIIB BENIN"},
   { id: 20, name: "Toyota Noah Si Gs Version EDGE", price: "0.0031", image:car14, shopName: "SIIB BENIN"},
   { id: 21, name: "Pre-Owned BMW X5 ", price: "0.39", image:car15, shopName: "SIIB BENIN"},
-  { id: 22, name: "2023 Toyota Hilux Revo Double Cab Z", price: "0.0034", image:car16, shopName: "SIIB Burundi"},
+  { id: 22, name: "House Celtigar", price: "0.0034", image:house7, shopName: "SIIB Burundi"},
+  { id: 23, name: "Blackberry Cottage", price: "0.0045", image:house8, shopName: "SIIB Burundi"},
+  { id: 24, name: "Chimney Cottage", price: "0.0061", image:house9, shopName: "SIIB TANZANIA"},
+  { id: 25, name: "Brahmins House", price: "0.0039", image:house10, shopName: "SIIB Burundi"},
+  { id: 26, name: "Oakhall Cottage", price: "0.0043", image:house11, shopName: "SIIB RWANDA"},
+  { id: 27, name: "Birch Cottage", price: "0.0061", image:house12, shopName: "SIIB RWANDA"},
+  { id: 28, name: "Rose Cottage", price: "0.0061", image:house13, shopName: "SIIB BURUNDI"},
+  { id: 29, name: "Barton Villa", price: "0.0061", image:house14, shopName: "SIIB RWANDA"},
+  { id: 30, name: "Ferrari", price: "0.0043", image:car17, shopName: "SIIB RWANDA"},
+  { id: 31, name: "Birch Cottage", price: "0.0061", image:car18, shopName: "SIIB TANZANIA"},
+  { id: 32, name: "Rose Cottage", price: "0.0071", image:car19, shopName: "SIIB RWANDA"},
+  { id: 33, name: "Barton Villa", price: "0.0061", image:car20, shopName: "SIIB SOUTH AFRICA"},
+  { id: 34, name: "Landcruiser V9", price: "0.0064", image:car33, shopName: "SIIB BURUNDI"},
+  { id: 35, name: "Landcruiser V9", price: "0.0069", image:houses, shopName: "SIIB BURUNDI"},
+  { id: 36, name: "Landcruiser V9", price: "0.0063", image:vpcar, shopName: "SIIB BURUNDI"},
+  { id: 37, name: "Landcruiser V9", price: "0.0062", image:vpcar2, shopName: "SIIB BURUNDI"},
+  { id: 38, name: "Jeep 2024", price: "0.0063", image:jeep, shopName: "SIIB TANZANIA"},
+  { id: 39, name: "Landlover 2024", price: "0.0081", image:landlover, shopName: "SIIB RWANDA"},
+  { id: 40, name: "Crown 2024", price: "0.0077", image:crown, shopName: "SIIB TANZANIA"},
+  { id: 41, name: "Mercedes-Benz Trucks", price: "0.00682", image:benz2, shopName: "SIIB BURUNDI"},
+  { id: 42, name: "Mercedes-Benz Trucks", price: "0.0084", image:benz, shopName: "SIIB RWANDA"},
+  { id: 43, name: "Toyota Prado 2024", price: "0.0084", image:prado, shopName: "SIIB RWANDA"},
+  { id: 44, name: "The New American Home 2024", price: "0.0087", image:house21, shopName: "SIIB RWANDA"},
+  { id: 45, name: "The New American Home ", price: "0.0087", image:house22, shopName: "SIIB RWANDA"},
+  { id: 46, name: "2Modern Luxury Home Building", price: "0.0089", image:house23, shopName: "SIIB BURUNDI"},
+  { id: 47, name: "Murray Homes", price: "0.0092", image:house24, shopName: "SIIB TANZANIA"},
+  { id: 48, name: "Murray Homes", price: "0.0092", image:house24, shopName: "SIIB TANZANIA"},
+  { id: 49, name: "Murray Homes", price: "0.0092", image:house24, shopName: "SIIB TANZANIA"},
+  { id: 50, name: "Murray moden house", price: "0.0095", image:house25, shopName: "SIIB TANZANIA"},
+  { id: 51, name: "Tropical House Design", price: "0.0097", image:house26, shopName: "SIIB TANZANIA"},
 ];
 
 export default function MyCard() {
