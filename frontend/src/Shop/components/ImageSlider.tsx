85858823081d5f6ce6d9e6dd4102e0250ae9e93d
regiss05 <regiss.steven@gmail.com>
2025-03-12 20:55:17 +0300

    fix

--- frontend/src/Shop/components/ImageSlider.tsx
+++ frontend/src/Shop/components/ImageSlider.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Box } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import SwipeableViews from "react-swipeable-views";
 
 // Import local images
@@ -9,10 +9,10 @@ import slide3 from "../../imges/statics/dapps.svg";
 import slide4 from "../../imges/statics/articles.svg";
 
 const images = [
-  { label: "Image 1", imgPath: slide1 },
-  { label: "Image 2", imgPath: slide2 },
-  { label: "Image 3", imgPath: slide3 },
-  { label: "Image 4", imgPath: slide4 },
+  { label: "Image 1", imgPath: slide1, buttonLabel: "Create Account", position: { bottom: "15%", left: "27%" } },
+  { label: "Image 2", imgPath: slide2, buttonLabel: "Create Shop", position: { bottom: "15%", left: "32%" } },
+  { label: "Image 3", imgPath: slide3, buttonLabel: "More...", position: { bottom: "15%", left: "26%" } },
+  { label: "Image 4", imgPath: slide4, buttonLabel: "More...", position: { bottom: "15%", left: "28%" } },
 ];
 
 export default function ImageSlider() {
@@ -29,17 +29,37 @@ export default function ImageSlider() {
 
   return (
     <Box sx={{ maxWidth: 800, flexGrow: 1, margin: "auto", textAlign: "center" }}>
-      <h4 className="cardforyou">#Promotions</h4>
-      <Box sx={{ margin: "1rem 0" }}>
-        <SwipeableViews index={activeStep} onChangeIndex={setActiveStep} enableMouseEvents >
+      <h4 className="cardforyou">#Updatenews</h4>
+
+      <Box sx={{ position: "relative", margin: "1rem 0" }}>
+        <SwipeableViews index={activeStep} onChangeIndex={setActiveStep} enableMouseEvents>
           {images.map((step, index) => (
-            <div key={index} style={{ display: "flex", justifyContent: "center" }}>
+            <div key={index} style={{ display: "flex", justifyContent: "center", position: "relative" }}>
               {Math.abs(activeStep - index) <= 2 ? (
-                <img
-                  src={step.imgPath}
-                  alt={step.label}
-                  style={{ width: "90%", borderRadius: "10px" }}
-                />
+                <>
+                  <img
+                    src={step.imgPath}
+                    alt={step.label}
+                    style={{ width: "90%", borderRadius: "10px" }}
+                  />
+                  {/* Button with unique position */}
+                  <Button
+                    variant="contained"
+                    sx={{
+                      position: "absolute",
+                      bottom: step.position.bottom,
+                      left: step.position.left,
+                      transform: "translateX(-50%)",
+                      backgroundColor: "orange",
+                      color: "white",
+                      borderRadius: "5px",
+                      textTransform: "none",
+                      "&:hover": { backgroundColor: "#e69500" },
+                    }}
+                  >
+                    {step.buttonLabel}
+                  </Button>
+                </>
               ) : null}
             </div>
           ))}
